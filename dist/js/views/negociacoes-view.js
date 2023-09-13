bce5c2525a376a7a42ad0c85cc21d11ab64e1a1c
Fernando de Lima <fernando.lima@ambevtech.com.br>
2023-09-13 19:03:49 -0300

    formatando data com Intl.DateTimeFormat().format()

--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -19,7 +19,7 @@ export class NegociacoesView {
           ${model.lista().map(negociacao => {
             return `
                     <tr>
-                      <td>?</td>
+                      <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
                       <td>${negociacao.quantidade}</td>
                       <td>${negociacao.valor}</td>
                       <td>${negociacao.volume}</td>
