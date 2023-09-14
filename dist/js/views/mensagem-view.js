a18f6c627808210e15f8bae5322fbe68a06e139a
Fernando de Lima <fernando.lima@ambevtech.com.br>
2023-09-14 10:04:12 -0300

    usando erança e modificador protected (apenas filhas tem acesso)

--- dist/js/views/mensagem-view.js
+++ dist/js/views/mensagem-view.js
@@ -1,12 +1,10 @@
-export class MensagemView {
-    constructor(selector) {
-        this.element = document.querySelector(selector);
-    }
+import { View } from "./view.js";
+export class MensagemView extends View {
     template(model) {
         return `<p class="alert alert-info">${model}</p>`;
     }
     update(model) {
         const template = this.template(model);
-        this.element.innerHTML = template;
+        this.elemento.innerHTML = template;
     }
 }
