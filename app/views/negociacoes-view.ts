cc2b561cacdd711534e0d7e3af8384fab91ee973
Fernando de Lima <fernando.lima@ambevtech.com.br>
2023-09-14 09:49:16 -0300

    add mensagem retorno usuario

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,46 +2,46 @@ import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacoesView {
 
-  private elemento: HTMLElement;
+    private elemento: HTMLElement;
 
-  constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
-  }
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
 
-  template(model: Negociacoes): string {
-    return `
-      <table class="table table-hover table-bordered">
-        <thead>
-          <tr>
-            <th>DATA</th>
-            <th>QUANTIDADE</th>
-            <th>VALOR</th>
-            <th>VOLUME</th>
-          </tr>
-        </thead>
-        
-        <tbody>
+    template(model: Negociacoes): string {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+                </thead>
+                
+                <tbody>
 
-          ${model.lista().map(negociacao => {
-            return `
-                    <tr>
-                      <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
-                      <td>${negociacao.quantidade}</td>
-                      <td>${negociacao.valor}</td>
-                      <td>${negociacao.volume}</td>
-                    </tr>
-                  `;
-          }).join('')}
+                ${model.lista().map(negociacao => {
+                    return `
+                            <tr>
+                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                            </tr>
+                        `;
+                }).join('')}
 
-        </tbody>
-      </table>
-    `;
-  }
+                </tbody>
+            </table>
+            `;
+    }
 
-  update(model: Negociacoes): void {
-    const template = this.template(model);
-    console.log(template);
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template);
 
-    this.elemento.innerHTML = template;
-  }
+        this.elemento.innerHTML = template;
+    }
 }
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -4,32 +4,32 @@ export class NegociacoesView {
     }
     template(model) {
         return `
-      <table class="table table-hover table-bordered">
-        <thead>
-          <tr>
-            <th>DATA</th>
-            <th>QUANTIDADE</th>
-            <th>VALOR</th>
-            <th>VOLUME</th>
-          </tr>
-        </thead>
-        
-        <tbody>
+            <table class="table table-hover table-bordered">
+                <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+                </thead>
+                
+                <tbody>
 
-          ${model.lista().map(negociacao => {
+                ${model.lista().map(negociacao => {
             return `
-                    <tr>
-                      <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
-                      <td>${negociacao.quantidade}</td>
-                      <td>${negociacao.valor}</td>
-                      <td>${negociacao.volume}</td>
-                    </tr>
-                  `;
+                            <tr>
+                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                            </tr>
+                        `;
         }).join('')}
 
-        </tbody>
-      </table>
-    `;
+                </tbody>
+            </table>
+            `;
     }
     update(model) {
         const template = this.template(model);
