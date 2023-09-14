17ce7090db8e7b7a392a811fa9d3d27162ac53ba
Fernando de Lima <fernando.lima@ambevtech.com.br>
2023-09-14 10:42:47 -0300

    usando classes e metodos abstratos

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
 
     protected elemento: HTMLElement;
 
@@ -11,7 +11,5 @@ export class View<T> {
         this.elemento.innerHTML = template;
     }
 
-    template(model: T): string {
-        throw new Error('Você deve implementar o método template');
-    }
+    abstract template(model: T): string;
 }
