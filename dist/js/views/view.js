e05317f3eaaf1d095cadfbcb0f9aa09a21ae881a
Fernando de Lima <fernando.lima@ambevtech.com.br>
2023-09-14 10:30:33 -0300

    usando generics em heranças

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,4 +2,11 @@ export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+    template(model) {
+        throw new Error('Você deve implementar o método template');
+    }
 }
